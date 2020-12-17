93105c4aae0efead515c4aa9c9a80ada0eb05cc4
Mohamed Lemine Ould El-Hadj <mohamedlemineelhadj@outlook.com>
2020-12-16 19:30:26 -0500

    Write common request listener function for all pendulum instances

--- back/app.js
+++ back/app.js
@@ -5,9 +5,10 @@ const Timer = require('./timer');
 const hostname = '127.0.0.1';
 const port = 3001;
 
-const timer = new Timer();
-// Seriously need to figure out how to duplicate this code
-const server = http.createServer((req, res) =>{
+const timer1 = new Timer();
+const timer2 = new Timer();
+
+const requestListener = function(req, res, timer){
     const url = req.url;
     if (url == '/start'){
         timer.start();
@@ -29,26 +30,20 @@ const server = http.createServer((req, res) =>{
         res.write(String(timer.getCurrentTimeElapsed()));
         res.end();
     }else{
-    res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.end();
-    //res.end(x.test('Another test'));
     }
-});
+}
 
-const server2 = http.createServer((req, res) =>{
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.end(x.test('This is a test'));
-});
-
-server.listen(port, hostname, () =>{
+const server = http.createServer((req,res) =>{
+    requestListener(req,res,timer1);
+}).listen(port, hostname, () =>{
     console.log(`Pendulum running at http://${hostname}:${port}/`);
 });
 
-server2.listen(port+1, hostname, () =>{
+const server2 = http.createServer((req, res) =>{
+    requestListener(req,res,timer2);
+}).listen(port+1, hostname, () =>{
     console.log(`Pendulum running at http://${hostname}:${port+1}/`);
 });
-
