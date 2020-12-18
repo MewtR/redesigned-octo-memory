2a26eeaf4d71e1a03b8795892d38afd633264679
Mohamed Lemine Ould El-Hadj <mohamedlemineelhadj@outlook.com>
2020-12-17 22:42:28 -0500

    Remove some console logs and some comments

--- back/helpers.js
+++ back/helpers.js
@@ -2,31 +2,13 @@ module.exports = {
     angularDisplacement: function(initialAngle, length, time){
         const gravitationalAcceleration = 9.8;
         const initialAngleInRads = toRadians(initialAngle);
-        /*
-        console.log("Time is: ", Math.round(time/1000));
-        const squirt = Math.sqrt(gravitationalAcceleration/length);
-        console.log("squirt: ", squirt);
-        const multiplied = squirt*(Math.round(time/1000));
-        console.log("multiplied: ", multiplied);
-        const sinm = Math.sin(multiplied);
-        console.log("sinm: ", sinm);
-        const result = sinm*initialAngleInRads;
-        console.log("result in rads: ", result);
-        console.log("result in degs: ", toDegrees(result));
-        */
-        const angle = toDegrees( initialAngleInRads * Math.sin( (Math.sqrt( gravitationalAcceleration/length ))*( Math.round(time/1000) ) ));
-        console.log("Angle is: ", angle);
-
+        const angle = toDegrees( initialAngleInRads * Math.sin( (Math.sqrt( gravitationalAcceleration/length ))*( time/1000 ) ));
         return angle;
     },
     coordinates: function(angle, length){
-        console.log("Length is: ", length);
-        console.log("Angle in degs: ", angle);
         const angleInRads = toRadians(angle);
-        console.log("Angle in rads:  ",  angleInRads);
         const x = length*Math.sin(angleInRads);
         const y = length - length*Math.cos(angleInRads);
-        console.log("x is: "+x +" and y is: "+y);
         return {x, y};
     }
 }
--- front/js/draw.js
+++ front/js/draw.js
@@ -32,41 +32,21 @@ function draw(pendulumNumber, length, x, y){
     const radius = 10;
     const canvas = $("#pendulum"+pendulumNumber);
     const ctx = canvas[0].getContext('2d');
+    //clear canvas
     ctx.clearRect(0,0, canvas[0].width, canvas[0].height);
     //Restoring context at the end of every draw allows to use to 
     // more easily set the point of origin to the pendulum at rest and 
     // also more easily clear the  previous drawings
     ctx.save();
-    console.log("Canvas width is: ", canvas[0].width);
-    console.log("Canvas is: ", canvas[0]);
     const canvasXMidpoint = Math.round(canvas[0].width * 0.5);
     const canvasYMidpoint = Math.round(canvas[0].height * 0.5);
     const lengthInPixels = convertMetersToPixels(length);
     const xInPixels = convertMetersToPixels(x);
     const yInPixels = convertMetersToPixels(y);
-    console.log("Length in pixels: ", lengthInPixels);
-    console.log("x in pixels: ", xInPixels);
-    console.log("y in pixels: ", yInPixels);
-    console.log("canvasXMidpoint: ", canvasXMidpoint);
-    console.log("canvasYMidpoint: ", canvasYMidpoint);
-    //clear canvas
-    //ctx.clearRect(-canvasXMidpoint,-canvasYMidpoint, canvas[0].width, canvas[0].height);
     ctx.beginPath();
     // Set origin to middle of the pendulum at rest
     ctx.translate(canvasXMidpoint, lengthInPixels);
-    // Rotate so that it's more intuitive
-    //ctx.rotate(2*Math.PI*(3/4)); // this is completely wrong because the x ends up pointing up and the y ends up pointing right
     ctx.scale(1,-1); // flip the y axis 
-
-    
-    /*
-    ctx.moveTo(0,0);
-    ctx.lineTo(0, 200);
-    ctx.moveTo(0,0);
-    ctx.lineTo(50, 0);
-    */
-    
-    
     
     //Draws an arc which is centered at xInPixels,yInPixels
     ctx.arc(xInPixels, yInPixels, radius, 0, 2*Math.PI);
